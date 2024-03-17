56d9f82821ec595ca6ca061416bf01c7021e65b6
PROMISE UZOR OKWUDIRI <iamupoprofessional@gmail.com>
2024-03-17 23:29:39 +0000

    UPDATE

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
-let next = document.querySelector('.next')
-let prev = document.querySelector('.prev')
+// let next = document.querySelector('.next')
+// let prev = document.querySelector('.prev')
 
-next.addEventListener('click', function(){
-    let items = document.querySelectorAll('.item')
-    document.querySelector('.slide').appendChild(items[0])
-})
+// next.addEventListener('click', function(){
+//     let items = document.querySelectorAll('.item')
+//     document.querySelector('.slide').appendChild(items[0])
+// })
 
-prev.addEventListener('click', function(){
-    let items = document.querySelectorAll('.item')
-    document.querySelector('.slide').prepend(items[items.length -1 ]) 
-})
+// prev.addEventListener('click', function(){
+//     let items = document.querySelectorAll('.item')
+//     document.querySelector('.slide').prepend(items[items.length -1 ]) 
+// })
+
+let next = document.querySelector('.next');
+let prev = document.querySelector('.prev');
+let slide = document.querySelector('.image-slide');
+
+next.addEventListener('click', function() {
+    let items = slide.querySelectorAll('.item');
+    slide.appendChild(items[0]);
+});
+
+prev.addEventListener('click', function() {
+    let items = slide.querySelectorAll('.item');
+    slide.prepend(items[items.length - 1]);
+});
