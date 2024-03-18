649dba48fe8186423d7eb83048023cdeaa087c15
PROMISE UZOR OKWUDIRI <iamupoprofessional@gmail.com>
2024-03-18 00:38:21 +0000

    update

--- index.js
+++ index.js
@@ -1,26 +1,24 @@
-// let next = document.querySelector('.next')
-// let prev = document.querySelector('.prev')
-
-// next.addEventListener('click', function(){
-//     let items = document.querySelectorAll('.item')
-//     document.querySelector('.slide').appendChild(items[0])
-// })
-
-// prev.addEventListener('click', function(){
-//     let items = document.querySelectorAll('.item')
-//     document.querySelector('.slide').prepend(items[items.length -1 ]) 
-// })
-
 let next = document.querySelector('.next');
 let prev = document.querySelector('.prev');
 let slide = document.querySelector('.image-slide');
+let sliderView = document.querySelector('.slider-view');
 
 next.addEventListener('click', function() {
     let items = slide.querySelectorAll('.item');
+    let nextItem = items[0].cloneNode(true);
     slide.appendChild(items[0]);
+    updateSliderViewBackground(items[1]);
 });
 
 prev.addEventListener('click', function() {
     let items = slide.querySelectorAll('.item');
-    slide.prepend(items[items.length - 1]);
+    let lastItem = items[items.length - 1].cloneNode(true);
+    slide.prepend(lastItem);
+    slide.removeChild(items[items.length - 1]);
+    updateSliderViewBackground(lastItem);
 });
+
+function updateSliderViewBackground(item) {
+    let backgroundImage = item.style.backgroundImage;
+    sliderView.style.backgroundImage = backgroundImage;
+}
